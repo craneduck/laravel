c6523309e93352fe8c2f1c632670e39a01b2e615
craneduck <28823828+craneduck@users.noreply.github.com>
2026-08-21 14:37:49 +0900

    ページタイトルを追加

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,7 +1,10 @@
 import { createInertiaApp } from '@inertiajs/react';
 import '../css/app.css';
 
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
 createInertiaApp({
+  title: (title) => (title ? `${title} - ${appName}` : appName),
   strictMode: true,
   pages: {
     path: './pages',
