8b04995f1dd07a233e612f3b261dd74caa6939c1
craneduck <28823828+craneduck@users.noreply.github.com>
2026-08-21 14:13:43 +0900

    add app.blade.php, createInertiaApp

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-//
+import { createInertiaApp } from '@inertiajs/react';
+import '../css/app.css';
+
+createInertiaApp({
+  strictMode: true,
+  pages: {
+    path: './pages',
+    extension: '.tsx',
+  },
+});
