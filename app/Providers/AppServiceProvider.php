a0ca75fb17b9cc967efc6f56cb4577614b827441
craneduck <28823828+craneduck@users.noreply.github.com>
2026-08-21 11:41:15 +0900

    update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +24,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Modelのエラーを検出
+        /** @var Application */
+        $app = $this->app;
+        Model::shouldBeStrict(! $app->isProduction());
+
+        // wayfinderでサブディレクトリを使うためにのためにAPP_URLをforceRootUrl()に設定
+        // https://github.com/laravel/wayfinder/pull/105
+        /** @var string */
+        $appUrl = config('app.url');
+        URL::forceRootUrl($appUrl);
     }
 }
