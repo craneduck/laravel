eb9df8f988f838edcfe2a48c2d82f84ab78034e5
craneduck <28823828+craneduck@users.noreply.github.com>
2026-08-21 12:55:11 +0900

    add Middleware/HandleInertiaRequests.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,10 +2,14 @@
 
 declare(strict_types=1);
 
+use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,10 +18,21 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->web(append: [
+            HandleInertiaRequests::class,
+            AddLinkHeadersForPreloadedAssets::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
         );
+
+        $exceptions->respond(function (Response $response) {
+            if ($response->getStatusCode() === 419) {
+                return Inertia::flash('message', 'ページの有効期限が切れました。もう一度入力してください')->back();
+            }
+
+            return $response;
+        });
     })->create();
